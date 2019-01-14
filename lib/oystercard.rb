f24bfc4b338f57db32e06947e7a6278b776de7b1
dangroze <dangroze@gmail.com>
2019-01-14 16:34:11 +0000

    Added top_up feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,10 @@
 class Oystercard
   attr_reader :balance
+
   def initialize()
     @balance = balance
+  end
+
+  def top_up
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,5 @@
 require 'oystercard'
 RSpec.describe Oystercard do
   it {is_expected.to respond_to(:balance)}
+  it {is_expected.to respond_to(:top_up)}
 end
